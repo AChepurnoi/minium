302778392f17e25068cf34a81b57f389579837d1
graniumHub <granium90@gmail.com>
2016-12-19 12:25:30 +0200

    Coursework

--- build/resources/main/migration/V1__Users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id       SERIAL PRIMARY KEY NOT NULL,
-  login    TEXT               NOT NULL,
-  password TEXT               NOT NULL,
-  role     TEXT               NOT NULL,
-  active   BOOLEAN DEFAULT FALSE
-);
-
--- build/resources/main/migration/V2__User_update.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users ADD COLUMN following JSONB;
-ALTER TABLE users ADD COLUMN followers JSONB;
